78e4251c6b5116a58ce03819b3497df8ec85fedb
Thomas Fett <user@thomasfett.de>
2014-02-08 19:34:51 +0100

    added result displaying for the game

--- nerdigalli.ino
+++ nerdigalli.ino
@@ -9,8 +9,9 @@ char ch;
 
 const int contrast = 15; // display contrast
 const int player = 2; // number of players
-const int space = 10; // space between the players / between player one and the sensor
+const int space = 1000; // space between the players / between player one and the sensor (in millimeters)
 const int interval = 1000; // interval between display changes in milliseconds
+const int ignoredDistance = 1000; // the sensor ignores everything above this distance (in millimeters)
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -25,6 +26,7 @@ int numberOfChangedSteps = 0;
 boolean finished = false; //game is finished
 boolean isRedOn = 0;
 boolean isGreenOn = 0;
+int intervenedPlayer = 0;
 
 void setup() 
 {
@@ -48,6 +50,11 @@ void loop()
 {
   if ((millis()/interval) > numberOfChangedSteps) {
     setNewValues();
+  }
+
+  if (/*TODO: sensor value*/ > ignoredDistance) {
+    intervenedPlayer = getIntervenedPlayerNumber(/*TODO: sensor value*/sensorDistance);
+    calculateResult();
   }
   
   lcd.setCursor(5, 2);
@@ -91,7 +98,7 @@ void setNewValues() {
   }
 
   numberOfChangedSteps++;
-  Serial.println(numberOfChangedSteps);
+  // Serial.println(numberOfChangedSteps);
 }
 
 void calculateResult()
@@ -117,7 +124,6 @@ void calculateResult()
     }
   }
 
-  // 
   if (isRedOn == false &&
       isGreenOn == true &&
       (numberOfAs == 3 ||
@@ -128,4 +134,51 @@ void calculateResult()
 
     displayResultMessage(winner);
   }
+}
+
+int getIntervenedPlayerNumber(int sensorDistance)
+{
+  for (int i = 1; i <= player; i++) {
+    if (i * space > sensorDistance) {
+      intervenedPlayer = i;
+    }
+  }
+}
+
+void displayResultMessage(gameIsWon)
+{
+  char resultString[] = "Player X is a ";
+  char resultTitle[6];
+
+  switch (intervenedPlayer) {
+      case 1:
+        resultString[7] = 49;
+        break;
+      case 2:
+        resultString[7] = 50;
+        break;
+      case 3:
+        resultString[7] = 51;
+        break;
+      case 4:
+        resultString[7] = 52;
+        break;
+      default:
+        // do something
+  }
+
+  if (gameIsWon) {
+      // Winner
+      resultTitle = "Winner";
+    } else {
+      //Loser!
+      resultTitle = "Loser ";
+    }
+  lcd.setCursor(5, 2);
+  lcd.print(resultString);
+  lcd.setCursor(5, 3);
+  lcd.print(resultTitle);
+  lcd.setCursor(5, 1)
+  lcd.print(displayString);
+  delay(20000);
 }
